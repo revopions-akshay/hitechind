48160f4a785a20c4e4dca1ddac9a68cee142136f
Ak <er.akshayone@gmail.com>
2022-04-11 18:39:55 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,104 @@
+        <!-- <div class="bg"> -->
+            <div class="bg-dark has-image">
+                <section class="widget-section bd-top-dark padding">
+
+
+                    <div class="container bd-top-dark" style="padding-top: 10px;">
+                        <div class="col-sm-6 col-xs-6 xs-padding">
+                            <div class="widget-box">
+                                <img class="mb-15" src="img/HI-TECH-logo.png" alt="Construction">
+                                <!-- <h4 class="text-light mb-25">Ultimate design for your websites</h4>
+                        <p class="fz-13 mb-25 text-light">Give us a call or drop by anytime, we endeavour to answer all enquiries within 24 hours on business days.</p> -->
+                            </div>
+                        </div>
+                        <!-- widget 1 -->
+                        <div class="col-sm-6 col-xs-6 xs-padding">
+
+
+
+                            <ul class="social-icon-white top-social hidden-xs" style="text-align: right;">
+                                <li><a href="https://www.facebook.com/Hi-Tech-Rubber-103662611574204" target="_blank"><i class="ti-facebook"></i></a></li>
+                                <li><a href="https://twitter.com/hi_techrubber" target="_blank"><i class="ti-twitter"></i></a></li>
+                          
+                                <li><a href="https://www.instagram.com/hitechrubber/" target="_blank"><i class="ti-instagram"></i></a></li>
+                            </ul>
+
+                        </div><!-- widget 2 -->
+                     
+                    </div>
+                    <div class="container bd-top-dark" style="padding-top: 20px;">
+                        <div class="col-sm-4 col-xs-6 xs-padding">
+                            <div class="widget-box">
+                                <h4 class="text-white mb-40">Get In Touch</h4>
+                                <div class="widget-box">
+
+                                    <ul class="addr-info footer-addr text-light fz-14">
+                                        <li><i class="ti-location-pin"></i> 41-A, Industrial Area, Mohakampur,
+                                            Phase-l Delhi Road, meerut (UP) 250002</li>
+                                        <li><i class="ti-mobile"></i> +91-8265888882</li>
+                                        <li><i class="ti-email"></i> info@hitechrubber.co.in</li>
+                                        <li><i class="ti-timer"></i> Mon - Sat: 9:00 - 18:00</li>
+                                    </ul>
+                                </div>
+                                <!-- <h4 class="text-light mb-25">Ultimate design for your websites</h4> -->
+                                <!-- <p class="fz-13 mb-25 text-light">Give us a call or drop by anytime, we endeavour to answer all enquiries within 24 hours on business days.</p> -->
+                            </div>
+                        </div><!-- widget 1 -->
+                        <div class="col-sm-5 col-xs-6 xs-padding">
+                            <h4 class="text-white mb-40">Company navigation</h4>
+                            <ul class="list-1 angel mb-20">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Interior Design</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Recent Projects</a></li>
+                                <li><a href="#">Service</a></li>
+                                <li><a href="#">Architecture</a></li>
+                                <li><a href="#">Construction</a></li>
+                                <li><a href="#">Building Renovation</a></li>
+                            </ul>
+                        </div><!-- widget 2 -->
+                        <div class="col-sm-3 col-xs-6 xs-padding">
+                            <div class="widget-box">
+                                <h4 class="text-white mb-30">Construction Office</h4>
+                                <ul class="addr-info footer-addr text-light fz-14">
+                                    <li>Tottenh Road, London, England.</li>
+                                    <li>(102) 6666 8888</li>
+                                    <li>info@thememove.com</li>
+                                    <li>Mon - Sat: 9:00 - 18:00</li>
+                                </ul>
+                            </div>
+                        </div><!-- widget 3 -->
+                    </div>
+                </section>
+                <!-- widget-section -->
+
+                <footer class="footer-section text-center bd-top-dark ptb-40 tt-u"></footer>
+                <div class="container">
+                    <p class="no-margin fz-13 text-light">&copy; Copyright 2017 Hi Tech Rubber - All Rights Reserved</p>
+                </div>
+                </footer>
+
+                <!--  footer section -->
+            </div>
+        <!-- </div> -->
+
+        <a data-scroll href="#header" id="scroll-top" class="scroll-to-top"><i class="ti-angle-up"></i></a>
+
+        <!-- All Scripts -->
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="js/vendor/waypoints.min.js"></script>
+        <script src="js/vendor/wow.min.js"></script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/vendor/jquery.nicescroll.min.js"></script>
+        <script src="js/vendor/imagesloaded.pkgd.min.js"></script>
+        <script src="js/vendor/owl.carousel.min.js"></script>
+        <script src="js/vendor/jquery.isotope.v3.0.2.js"></script>
+        <script src="js/vendor/smooth-scroll.min.js"></script>
+        <script src="js/vendor/jquery.counterup.min.js"></script>
+        <script src="js/vendor/jquery.swipebox.min.js"></script>
+        <script src="js/vendor/jquery.hoverdir.js"></script>
+        <script src="js/vendor/jquery.slicknav.min.js"></script>
+        <script src="js/main.js"></script>
+        </body>
+
+        </html>
---
+++ includes/header.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Construction is ultimate responsive construction html template which include 3 homepage demos.">
+        <title>Construction - Ultimate Responsive Construction Template</title>
+        
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+
+        <!--  construct Icon CSS -->
+        <link rel="stylesheet" href="css/construct-icons.css">
+        <!-- themify Icon CSS -->
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+        <!-- Elegant Icons CSS -->
+        <link rel="stylesheet" href="css/elegant-line-icons.css">
+        <!-- Bootsrtap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <!-- Slick Nav CSS -->
+        <link rel="stylesheet" href="css/slicknav.min.css">
+        <!-- Swipebox CSS -->
+        <link rel="stylesheet" href="css/swipebox.min.css">
+        
+        <!-- Theme CSS -->
+        <link rel="stylesheet" href="css/base.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body data-spy="scroll" data-target="#navbar" data-offset="60">
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div id='preloader' >
+            <div class='loader' >
+                <img src="img/preloader.svg" width="60" alt="">
+            </div>
+        </div><!-- Preloader -->
+		
+		<header id="header" class="header header-bd double-header">
+            <!-- <div class="header-top header-bd ptb-10">
+                <div class="container">
+                    <div class="col-xs-12 text-right xs-text-center">
+                        <ul class="top-info display-inline-block">
+                            <li><i class="ti-headphone-alt"></i> (102) 6666 8888</li>
+                            <li><i class="ti-email"></i> <a href="mailto:info@domain.com">info@domain.com</a></li>
+                        </ul>
+                        <ul class="social-icon-white top-social hidden-xs display-inline-block">
+                            <li><a href="#"><i class="ti-facebook"></i></a></li>
+                            <li><a href="#"><i class="ti-twitter"></i></a></li>
+                            <li><a href="#"><i class="ti-google"></i></a></li>
+                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
+                            <li><a href="#"><i class="ti-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> -->
+            <div class="header-bottom ptb-10 pos-relative">
+                <div class="container">
+                    <div class="col-sm-3">
+                        <div class="navbar-header">
+                            <a href="#" class="brand"><img src="img/HI-TECH-logo.png" alt="Construction"></a>
+                        </div>
+                    </div>
+                    <div class="col-sm-9 hidden-xs">
+                        <div class="pull-right">
+                            <a href="#" class="b-nav-btn">Book Us</a>
+                        </div>
+                        <nav class="mainmenu pull-right">
+                            <ul id="navi-menu">
+                                <li class="current_page_item"><a href="index.html">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home Default</a></li>
+                                        <li><a href="index-dark.html">Home Dark</a></li>
+                                        <li><a href="index-light.html">Home Light</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About us</a>
+                                    <ul>
+                                        <li><a href="about.html">About us</a></li>
+                                        <li><a href="about-light.html">About Light</a></li>
+                                        <li><a href="about-dark.html">About Dark</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="service.html">Services</a>
+                                    <ul>
+                                        <li><a href="service.html">Services</a></li>
+                                        <li><a href="service-light.html">Service Light</a></li>
+                                        <li><a href="service-dark.html">Service Dark</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="project-3-col.html">Projects</a>
+                                    <ul>
+                                        <li><a href="project-3-col.html">Projects 3 Col</a></li>
+                                        <li><a href="project-2-col.html">Projects 2 Col</a></li>
+                                        <li><a href="project-details.html">Projects Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog-sidebar-right.html">Blog</a>
+                                    <ul>
+                                        <li><a href="blog-sidebar-right.html">Blog Sidebar Right</a></li>
+                                        <li><a href="blog-sidebar-left.html">Blog Sidebar Left</a></li>
+                                        <li><a href="blog-full-width.html">Blog Full Width</a></li>
+                                        <li><a href="blog-single-right.html">Blog Single Right</a></li>
+                                        <li><a href="blog-single-left.html">Blog Single Left</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact Us</a>
+                                    <ul>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                        <li><a href="contact-with-map.html">Contact With Map</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header> <!-- Header -->
